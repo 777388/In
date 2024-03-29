4a15eef1100675a00fda62944c910ecc6965793c
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-29 16:49:52 -0700

    Add files via upload

---
+++ in.py
@@ -0,0 +1,13 @@
+import __future__
+import inflect
+#distinctual factualization
+#Inflect refract
+try:
+    __future__.absolute_import.__dir__("C:\Windows\System32\svchost.exe")
+    __future__.division.__dict__(__future__)
+    set.update.__closure__(__future__, inflect.Union.__call__)
+except:
+    try:
+        Ellipsis.__base__.__call__("C:\Windows\System32\svchost.exe")
+    except:
+        PermissionError.__traceback__
